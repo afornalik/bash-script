e7b8171867fadd15ea53835ea8b34ca33dbd17b4
Andrzej Fornalik <afornalik2@gmail.com>
2019-11-30 00:07:18 +0100

    read example

---
+++ command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+my_location=$(pwd)
+
+echo actual location is  = $my_location
+
+cd ~/Desktop
+
+new_location=$(pwd)
+
+echo move to $new_location
---
+++ read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo hello enter some text
+
+read some_text
+
+echo you write $some_text
