7b911c84758bd0a90a414c89493207935d36c3bc
Andrzej Fornalik <afornalik2@gmail.com>
2019-11-30 23:17:04 +0100

    second version of search_file script

--- search_file.sh
+++ search_file.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 echo file name or file name part  : $1
-echo located in : $2
 
 if [ -v $2 ]
 then 
-	looking_file=$(find . -name *$1*)
-	echo file located $looking_file
+	searched_file=$(find . -maxdepth 0 -name *$1* 2>/dev/null)
+	if [ -v $searched_file ]
+	then
+		searched_file=$(find . -name *$1* 2>/dev/null)
+	fi
 else
-	looking_file2=$(find $2 -name *$1*)
-	echo file located $looking_file2 in directory $2
+	searched_file=$(find $2 -maxdepth 0 -name *$1* 2>/dev/null)
+	if [ -v $searched_file ]
+	then
+		searched_file=$(find $2 -name *$1* 2>/dev/null)
+	fi
+fi
+
+if [ -v $searched_file ]
+then
+	echo file not exist
+else
+	echo file exist : $searched_file
 fi
 
 
